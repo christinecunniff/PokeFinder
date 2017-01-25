5ddccc53d6364077c734d9ac621d3c6a1baef485
Christine Cunniff <christinecunniff@gmail.com>
2017-01-25 11:56:20 -0500

    Main tutorial completed.

---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Christine Cunniff on 1/20/17.
+//  Copyright © 2017 Christine Cunniff. All rights reserved.
+//
+
+#import "GeoFire.h"
